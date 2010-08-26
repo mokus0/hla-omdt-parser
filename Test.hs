134b3b8ca639554283e1346e5af9821674a3bf0b
James Cook <james.cook@usma.edu>
2010-08-26 16:30:47 +0000

    Improved test app a bit

--- Test.hs
+++ Test.hs
@@ -7,22 +7,52 @@ import Text.Parsec
 
 import qualified Data.Map as M
 import qualified Data.IntMap as I
-
-srcName = "fom_v4.2_17NOV2008.omd"
+import System.Environment
 
 main = do
-    src <- readFile srcName
-    let tokens = alexScanTokens src
+    args <- getArgs
+    sequence_
+        [ do
+            src <- readFile srcName
+            let tokens = alexScanTokens src
     
-    case runParser omdt () srcName tokens of
-        Left err -> do
-            -- mapM_ print tokens
-            print err
-        Right objectModel -> do
-            sequence_
-                [ do
-                    putStr (key ++ ": ")
-                    print val
-                    putStrLn ""
-                | (key, val) <- M.assocs (routingSpaces objectModel)
-                ]
+            case runParser omdt () srcName tokens of
+                Left err -> do
+                    -- mapM_ print tokens
+                    print err
+                Right objectModel -> do
+                    let section heading = do
+                            putStrLn ""
+                            putStrLn ("    " ++ heading)
+                            putStrLn ("    " ++ map (const '-') heading)
+                            putStrLn ""
+                        dumpMap showKey kvs = sequence_
+                            [ do
+                                putStr (showKey key ++ ": ")
+                                print val
+                                putStrLn ""
+                            | (key, val) <- kvs
+                            ]
+            
+                    section "Object Model Header"
+                    print (header objectModel)
+            
+                    section "Enumerated Data Types"
+                    dumpMap id (M.assocs (enumeratedDataTypes objectModel))
+            
+                    section "Complex Data Types"
+                    dumpMap id (M.assocs (complexDataTypes objectModel))
+            
+                    section "Classes"
+                    dumpMap show (I.assocs (classes objectModel))
+            
+                    section "Interactions"
+                    dumpMap show (I.assocs (interactions objectModel))
+            
+                    section "Routing Spaces"
+                    dumpMap id (M.assocs (routingSpaces objectModel))
+            
+                    section "Notes"
+                    dumpMap show (I.assocs (notes objectModel))
+        | srcName <- args
+        ]
